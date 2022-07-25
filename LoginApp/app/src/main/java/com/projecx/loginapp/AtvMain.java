09466b09c92915536abec4050ce3599c89b6b773
김명준 <kmjkth1@naver.com>
2022-07-25 17:56:02 +0900

    아이디 비교해서 로그인 한다음 nextpage로 이동

--- LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
@@ -2,13 +2,36 @@ package com.projecx.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AtvMain extends AppCompatActivity {
+
+    EditText editID;
+    Button loginbutton;
+    String logininfo = "asdf";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.atv_main);
+
+        editID = findViewById(R.id.editID);
+        loginbutton = findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editID.getText().toString().equals(logininfo))
+                {
+                    Intent intent = new Intent(AtvMain.this , AvtNextPage.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
