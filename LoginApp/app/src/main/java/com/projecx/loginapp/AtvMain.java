db8658d96af53adf32aeae0bc3f183d1131555e9
LAPTOP-M1L11JAG\thsau <thsaudgus72@gmail.com>
2022-07-26 13:48:09 +0900

    [And] Login1- ID 로그인 -> NextPage

--- LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AtvMain.java
@@ -2,13 +2,50 @@ package com.projecx.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AtvMain extends AppCompatActivity {
 
+    String exmpId = new String("id");
+    EditText etId;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.atv_main);
+
+        InitLayout();
+        InitEvent();
     }
+
+    private void InitLayout()
+    {
+        etId = (EditText)findViewById(R.id.editID);
+        btnLogin = (Button)findViewById(R.id.loginbutton);
+
+    }
+
+    private void InitEvent()
+    {
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AtvMain.this, AvtNextPage.class);
+                if(exmpId.compareTo(etId.getText().toString())==0) {
+                    startActivity(intent);
+                }
+                else {
+                    Log.e("asdf","씨발");
+                }
+
+            }
+        });
+    }
+
 }
---
+++ LoginApp/app/src/main/java/com/projecx/loginapp/AvtNextPage.java
@@ -0,0 +1,14 @@
+package com.projecx.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvtNextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avt_next_page);
+    }
+}
